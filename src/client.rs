21acbd1fc719c202c1571e335364fc014fe0692f
arvchahal <arnavchahal14@gmail.com>
2025-10-12 11:51:57 -0500

    client basics

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,22 @@
+use std::str::FromStr;
+
 use reqwest::Client;
 use crate::markets::models::Account;
 
-const KALSHI_: &str = "";
-const KLASHI_ME: &str = "";
-const KLASHI_API_KEYS: &str = "";
+const KALSHI_API: &str = "https://api.elections.kalshi.com/trade-api/v2";
 
 struct KalshiClient{
+    http_client: Client,
+    account: Account,
+    base_url: String
+}
 
+impl KalshiClient{
+    pub fn new(user:Account) -> KalshiClient{
+        KalshiClient{
+            http_client: Client::new(),
+            account:user,
+            base_url: KALSHI_API.to_string(),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod markets;
 pub mod auth;
+pub(crate) mod client;
