08a7ddfc01a76238ae1b8315d0b3e8d79ba03ff1
arvchahal <arnavchahal14@gmail.com>
2025-10-15 13:56:04 -0500

    adding new endpoints

---
+++ src/api_reference/endpoints.rs
@@ -0,0 +1,4 @@
+const GET_CACHED_HISTORY:&str = "trade-api/v2/cached/events/{}/forecast_history";//no auth GET cached events forecast history replace brackets with ticker 
+const GET_EXCHANGE_ANNOUNCEMENTS: &str ="trade-api/v2/exchange/announcements";
+const GET_EXCHANGE_HISTORY: &str ="trade-api/v2/exchange/schedule";
+const GET_EXCHANGE_STATUS:&str = "trade-api/v2/exchange/status"
---
+++ src/exchange/endpoints.rs
@@ -0,0 +1,4 @@
+const GET_EXCHANGE_ANNOUNCEMENTS: &str ="trade-api/v2/exchange/announcements"; //no auth GET
+const GET_EXCHANGE_HISTORY: &str ="trade-api/v2/exchange/schedule"; //no auth GET
+const GET_EXCHANGE_STATUS:&str = "trade-api/v2/exchange/status"; //no auth GET
+const GET_USER_DATA_TIMESTAMP: &str = "trade-api/v2/exchange/user_data_timestamp"; //no auth GET
